7d7831bfa3df8ec34f5a70de85e5be57e6735063
inchKang <dlscjf1202@gmail.com>
2020-09-15 22:49:25 +0900

    로그인 기능 jwt 토큰 발급까지 완료.

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const {mongoSetting} = require('./config/key')
 const {User} = require("./models/User")
 const mongoose =require('mongoose')
 const bodyParser = require('body-parser')
-
-console.log(mongoSetting)
+const cookiePaarser = require('cookie-parser')
 
 //for application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
 //for application/json
 app.use(bodyParser.json());
+//for cookie-parser
+app.use(cookiePaarser());
 
 mongoose.connect(`mongodb://${mongoSetting.ip}/${mongoSetting.db}`,{
   useNewUrlParser:true,
@@ -47,6 +48,57 @@ app.post('/register',(req,res)=>{
   })
   
 })
+
+app.post('/login',(req,res)=>{
+
+  //요청된 이메일을 DB에서 찾는다.
+
+  User.findOne({email:req.body.email},(err,user)=>{
+    if(!user){
+      return res.json(
+        {
+          loginSuccess:false,
+          message:"제공된 이메일에 해당하는 유저가 없습니다."
+        })
+    }
+  //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+  user.comparePassword(req.body.password,(err,isMatch)=>{
+
+    //만약 에러가 있다면
+
+    if(err) return res.status(400).send(err);
+
+    if(!isMatch)
+    return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."});
+
+    user.generateToken((err,user)=>{
+      if(err) return res.status(400).send(err);
+
+      //에러가 없다면 토큰을 저장해야 함. 프론트단에서, 쿠키나, 로컬스토리지 등.
+      //지금은 쿠키에 저장함.
+      //이렇게 하면 프론트 단에 x_auth라는 이름의 쿠키가 저장되게 되고, json 으로 결과를 보여준다.
+      res.cookie("x_auth",user.token)
+      .status(200)
+      .json({loginSuccess:true,userId:user._id})
+
+
+    })
+
+
+
+    
+
+  })
+
+
+  })
+
+
+
+  //비밀번호까지 맞다면 토큰을 생성한다.
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const bcrpyt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
   name: {
@@ -40,7 +42,7 @@ userSchema.pre("save", function(next){
 
   //클라이언트에서 보낸 user 정보 =this 와 동일.
   const user = this;
-  //비밀번호가 변경될 때만 암호화를 해준다. -> 모델 안에 필드에 패스워드가 있을떄.
+  //비밀번호가 변경될 때만 암호화를 해준다. -> 스키마에 필드에 패스워드가 있을떄.
   if(user.isModified('password')){
   //salt를 만든다. 만들때는 saltRound가 필요, 10 이면 10자의 salt로 암호화.
   bcrpyt.genSalt(saltRounds, (err, salt)=>{
@@ -57,6 +59,37 @@ userSchema.pre("save", function(next){
     next();
   }
 });
+
+userSchema.methods.comparePassword = function(plainPassword,callback){
+
+  //그냥 비밀번호와 암호화된 비밀번호가 동일한지 체크.
+  //bcrypt의 compare 을 통해 비교,
+  bcrpyt.compare(plainPassword,this.password, function(err,isMatch){
+    if(err) return callback(err)
+
+    //성공시 에러는 null, isMatch는 true로 결과가 나올것. 
+    callback(null,isMatch)
+  })
+
+
+
+}
+
+userSchema.methods.generateToken=function(callback){
+
+  const user =this;
+  const token = jwt.sign(user._id.toHexString(),'secretToken');
+
+  //jwt 인증 방식은 뒤에 있는 비밀 키를 통해 앞의 저장한 값을 가져 올 수 있다.
+  //만들어지는 방식은 앞에 등록한 객체 혹은 문자열 + 뒤의 시크릿 키값
+  user.token = token;
+  user.save(function(err,user){
+    if(err) return callback(err);
+
+    callback(null,user)
+  })
+
+}
 
 const User = mongoose.model("User", userSchema);
 
